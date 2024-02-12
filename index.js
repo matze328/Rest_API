3d6e39fb8a2331cdc1fa2ef6ffad0ddb9c4b54fe
matze328 <152269645+matze328@users.noreply.github.com>
2024-02-12 14:33:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require("cors")
+
+const { PORT } = process.env;
+const app = express();
+
+app.use(cors());
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ profile: { name: "Max"}});});
+app.get('/home', (req, res) => {
+    res.send("HEllo this is my home page");
+app.get('/user', (req, res) => {
+    res.status(200).json({ profile: { firstname: "Max", lastName: "Büscher", adresse: "Hengstenbergweg 2", hobbies: "Downhill" }});});
+app.get('/todoes', (req, res) => {
+    res.status(200).json({ profile: { todoe1: "Wäsche waschen", todoe2: "aufräumen" }});});
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
