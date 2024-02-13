b0bdd8558c3abd968cc66b113faa80bd6cc9ace6
matze328 <152269645+matze328@users.noreply.github.com>
2024-02-13 11:20:53 +0100

    todoes angepasst

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const app = express();
 
 app.use(cors());
 
+const todoes = [
+  {
+    id: 1,
+    todoe: "Wäsche waschen",
+  },
+  {
+    id: 2,
+    todoe: "Müll rausbrigen",
+  },
+];
+
 app.get('/test', (req, res) => {
   res.status(200).json({ profile: { name: "Max"}});});
 
 app.get('/user', (req, res) => {
   res.status(200).json({ profile: { firstname: "Max",lastName: "Büscher", adresse: "Hengstenbergweg 2", hobbies: "Downhill"}});});
 app.get('/todoes', (req, res) => {
-    res.status(200).json({ profile: { todoe1: "Wäsche waschen", todoe2: "aufräumen" }});});
+    res.json({todoes})
+}),
+app.get("/todoe", (req, res) => {
+  const todoeId = parseInt(req.query.todoeId);
+  const userProfile = todoes.find((item) => item.id === todoeId);
+  res.json({ todoe: userProfile });
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
